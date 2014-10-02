23390cdd68477b821ce905fd05f7be181a144900
Hayk Saakian <fakest.id.ever@gmail.com>
2014-10-02 14:56:09 -0700

    actually initialize the best in place javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require best_in_place
 //= require_tree .
+
+$(document).ready(function() {
+  /* Activating Best In Place */
+  jQuery(".best_in_place").best_in_place();
+});
